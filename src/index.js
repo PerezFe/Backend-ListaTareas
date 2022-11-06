467b42e8fe80eae74e4be5ee3e3e5ff6c5f24644
PerezFe <PerezFernandoE83@gmail.com>
2022-11-06 16:00:27 -0300

    agregamos los middlewares necesarios

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import path from "path";
 
 const app  = express();
 
 app.set("port", process.env.PORT || 4000);
 app.listen(app.get("port"),()=>{
-    console.log("Estoy en el puerto "+ app.get("port"))
+    console.log("Conectado en el puerto "+ app.get("port"))
 })
 
-console.log("Conectado");
+//----------- middlewares --------------
+app.use(morgan("dev"));
+//permitir peticiones remotas
+app.use(cors());
+//interpretar los objetos JSON
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+//cargar un archivo estatico
+app.use(express.static(path.join(__dirname, "../public")))
+
+
+//-------------- rutas: nombre de dominio -------------------
+// http://localhost:4000/
+app.get("/prueba", (req, res)=>{
+    res.send("Esto es una prueba de la peticion GET")
+})
